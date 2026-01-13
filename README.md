6e83e01eaa6cea7b881ba0251ea5e6920471c878
Elitsa Angelska <elitsaangelsca@gmail.com>
2026-01-13 16:44:31 +0200

    Initial project structure

---
+++ README.md
@@ -0,0 +1,7 @@
+# UP Course Project
+
+Course project for Introduction to Programming
+Faculty of Mathematics and Informatics
+Sofia University
+
+Compiler: GCC
---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+/**
+*  
+* Solution to course project # 04
+* Introduction to programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* Winter semester 2025/2026
+*
+* @author Elitsa Angelska
+* @idnumber 1MI0600556
+* @compiler GCC
+*
+* main file
+*
+*/
+
+int main() {
+	return 0;
+}
